889407d56ffa68196319220f93f5f40559f1d74c
joaovitorjulianifranco <74316161+joaovitorjulianifranco@users.noreply.github.com>
2023-01-20 10:44:42 -0300

    Update Exibir_senha.ts

--- Exibir_senha.ts
+++ Exibir_senha.ts
@@ -1,9 +1,17 @@
-function togglePassword(){
-const togglePassword = document.querySelector("password");
-const password = document.querySelector("#password");
-const fielpassword = document.getElementById("field-password"); //acessa o id do html, armazenando na constante
-
-
+ const togglePassword = document.querySelector("#password-toggle");
+                        const password = document.querySelector("#password");
+                        togglePassword.addEventListener("click", function(click){
+                            // Alternar atributo
+                            const type = password.getAttribute("type") === "password" ? "text" : "password";
+                                password.setAttribute("type", type);
+                            // Alternar o icone ao Mostrar a senha
+                            this.classList.toggle("bi-eye");
+                        });
+                            //impedir envio de formulário
+                            const formulário = document.querySelector("#formulário");
+                            form.addEventListener("submit", function (e){
+                                e.preventDefault();
+                            });
 
 
 
