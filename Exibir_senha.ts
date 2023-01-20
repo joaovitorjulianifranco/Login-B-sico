6a7650601f2156d634c78487735fcaa2c37c6ddd
joaovitorjulianifranco <74316161+joaovitorjulianifranco@users.noreply.github.com>
2023-01-20 10:41:47 -0300

    Em desenvolvimento

---
+++ Exibir_senha.ts
@@ -0,0 +1,29 @@
+function togglePassword(){
+const togglePassword = document.querySelector("password");
+const password = document.querySelector("#password");
+const fielpassword = document.getElementById("field-password"); //acessa o id do html, armazenando na constante
+
+
+
+
+
+
+//#--public binding() {
+//   for (const event of this.triggeringEvents) {
+//     this.node.addEventListener(event, this);
+//    }
+//  }
+//   aurelia/aurelia/validate-binding-behavior.spec.ts
+   
+//  add(name) {
+//    for (let i = 0; i < events[name].length; i++) {
+//      element.addEventListener(name, events[name][i]);
+//    }
+//  }
+//   jinkelajs/jinkela/AttributesManager.ts
+   
+//  private bindBeautifyEvent(el: HTMLInputElement) {
+//    el.nextElementSibling!.addEventListener('click', () => {
+//      el.click()
+//    })
+//}
