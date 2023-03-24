f9a3bae30724b1836fa59bd7e98246ca449e0f01
joaovitorjulianifranco <74316161+joaovitorjulianifranco@users.noreply.github.com>
2023-03-24 11:49:27 -0300

    Create Bota.js

---
+++ Bota.js
@@ -0,0 +1,5 @@
+const button = document.getElementById("button");
+/*Interaçao ao Clicar no botao para acessar a pagina seguinte*/
+button.myonclick = function(){
+    window.location.href = "#";
+}
